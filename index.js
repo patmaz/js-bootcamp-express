fb6cb53372d2b974c57f463a12b700db5bae8a32
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-03-01 20:18:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+//middleware
+app.use('/post/json', bodyParser.json());
+app.use('/post/json', function(req, res, next){
+    console.log('middle for /post/json', req.body);
+    next();
+});
+app.use('/post/form', bodyParser.urlencoded({ extended: false }));
+app.use('/post/form', function(req, res, next){
+    console.log('middle for /post/form', req.body);
+    next();
+});
+
+//GET
+// according to query params
+app.get('/get', function(req, res){
+    var query = req.query.q;
+    var path;
+
+    if(query === 'json') {
+        path = './data.json';
+    } else if (query === 'forms') {
+        path = './forms.txt';
+    }
+
+    fs.readFile(path, 'utf8', function(err, data){
+        if (err) throw err;
+        res.send(data);
+    });
+});
+
+//POST
+// save posts to data.json
+app.post('/post/json', function(req, res){
+        fs.readFile('./data.json', 'utf8', function(err, data){
+            if (err) throw err;
+            var dataParsed = JSON.parse(data);
+            dataParsed.items.push(req.body);
+
+            fs.writeFile('./data.json', JSON.stringify(dataParsed), 'utf8', function(err){
+                if (err) throw err;
+                res.send('saved');
+            });
+        });
+});
+
+// save forms to forms.txt
+app.post('/post/form', function(req, res){
+    var dataToSave = JSON.stringify(req.body) + '\n';
+    fs.appendFile('./forms.txt', dataToSave, 'utf8', function(err){
+        if (err) throw err;
+        res.send('saved');
+    });
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('nope!');
+});
