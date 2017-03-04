7ce28a7160695056973de5f485274c312be2ea7f
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-03-04 13:11:34 +0100

    first working version

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var config = require('./config.js');
-var passport = require('passport');
-var GoogleStartegy = require('passport-google-oauth').OAuth2Strategy;
-var googleProfile = {};
 var eventEmitter = require('events').EventEmitter;
 var emitter = new eventEmitter();
+var stormpath = require('express-stormpath');
 
 emitter.on('err', function(err){
     console.error('##### ERROR #####', err);
@@ -23,29 +21,13 @@ app.use('/post/json', function(req, res, next){
     next();
 });
 
-//passport
-passport.serializeUser(function(user, done){
-    done(null, user);
-});
-passport.deserializeUser(function(user, done){
-    done(null, user);
-});
-passport.use(new GoogleStartegy({
-        clientID: config.CLIENT_ID,
-        clientSecret:config.SECRET_KEY,
-        callbackURL: config.CALLBACK_URL
-    },
-    function(accessToken, refreshToken, profile, cb){
-        googleProfile = {
-            id: profile.id,
-            displayName: profile.displayName,
-            photo: profile.photos[0].value
-        };
-        cb(null, profile);
+app.use(stormpath.init(app, {
+    expand: {
+        customData: true
     }
-));
-app.use(passport.initialize());
-app.use(passport.session());
+}));
+
+app.use('/profile', stormpath.loginRequired, require('./profile')());
 
 // Templates
 app.set('view engine', 'pug');
@@ -54,68 +36,28 @@ app.set('views','./views');
 //static
 app.use(express.static('static'));
 
-app.get('/', function(req, res){
+//MAIN
+app.get('/', stormpath.getUser, function(req, res){
     res.render('index');
 });
 
-app.get('/auth/google',
-    passport.authenticate('google', {
-        scope : ['profile', 'email']
-    }
-));
-app.get('/auth/google/callback',
-    passport.authenticate('google', {
-        successRedirect : '/posts',
-        failureRedirect: '/'
-    }
-));
-
-//GET
-// according to query params
-app.get('/get', function(req, res){
-    var query = req.query.q;
-    var path;
-
-    if(query === 'booyah') {
-        path = __dirname + '/static/booyah.jpg';
-    } else {
-        path = __dirname + '/data.json';
-    }
-
-    res.sendFile(path);
-});
-
-// GET
-// feedback values from form
-app.get('/form', function(req, res){
-    res.sendFile(__dirname + '/static/form.html');
-});
-app.get('/feedback', function(req, res){
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.end(JSON.stringify(response));
-});
-
 // POSTS
-app.get('/addpost', function(req, res){
-    res.render('addpost', { user: googleProfile});
+app.get('/addpost', stormpath.getUser, function(req, res){
+    res.render('addpost');
 });
 
-app.get('/posts', function(req, res){
+app.get('/posts', stormpath.getUser, function(req, res){
     fs.readFile('./data.json', 'utf8', function(err, data){
         if (err) emitter.emit('err', new Error(err));
         res.render('posts', {
             title: 'Posts',
             posts: JSON.parse(data).items,
-            user: googleProfile
         });
     });
 });
 
 app.get('/:postid', function(req, res){
-    fs.readFile('./data.jsonn', 'utf8', function(err, data){
+    fs.readFile('./data.json', 'utf8', function(err, data){
         if (err) emitter.emit('err', new Error(err));
 
         var post = JSON.parse(data).items.filter(
